45c83ca53c574b431c4fa82674b31386c93ffa2c
yxosm <aosmanyx@gmail.com>
2025-03-23 06:51:55 -0400

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps } from "next-themes"
+import { createContext, useContext, useEffect, useState } from "react"
+
+const TouchContext = createContext({ isTouchDevice: false })
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const [isTouchDevice, setIsTouchDevice] = useState(false)
+
+  useEffect(() => {
+    setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0)
+  }, [])
+
+  return (
+    <TouchContext.Provider value={{ isTouchDevice }}>
+      <NextThemesProvider {...props}>
+        {children}
+      </NextThemesProvider>
+    </TouchContext.Provider>
+  )
+}
+
+export function useIsTouch() {
+  return useContext(TouchContext)
 }
